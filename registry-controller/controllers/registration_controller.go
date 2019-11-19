8728ac26481fdd604bdbda15974e74cf3c20b1a0
Ollie Hughes <olli.hughes@gmail.com>
2019-11-19 13:28:14 +0000

    Kubebuilder scaffolding for registry controller

---
+++ registry-controller/controllers/registration_controller.go
@@ -0,0 +1,52 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	cloudv1beta1 "github.com/spring-cloud/spring-cloud-kubernetes/registry-controller/api/v1beta1"
+)
+
+// RegistrationReconciler reconciles a Registration object
+type RegistrationReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=cloud.spring.io,resources=registrations,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cloud.spring.io,resources=registrations/status,verbs=get;update;patch
+
+func (r *RegistrationReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	_ = context.Background()
+	_ = r.Log.WithValues("registration", req.NamespacedName)
+
+	// your logic here
+
+	return ctrl.Result{}, nil
+}
+
+func (r *RegistrationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&cloudv1beta1.Registration{}).
+		Complete(r)
+}
